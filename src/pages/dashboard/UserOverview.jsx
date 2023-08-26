7fee762765282b992a63a13230b3991dea63fb36
Faleye Oluwafemi <faleyeoluwafemi1@gmail.com>
2023-08-27 00:21:50 +0100

    Add more components

--- src/pages/dashboard/UserOverview.jsx
+++ src/pages/dashboard/UserOverview.jsx
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Dashboard from "./Dashboard";
+import images from "../../constants/images";
 
 const UserOverview = () => {
   return (
     <>
       <Dashboard>
-        <div>User Overview</div>
+        <div className="">
+          <div className="bg-white w-full shadow-md rounded-[8px] py-[16px] px-[20px] flex items-center justify-between">
+            <div className="">
+              <div>
+                <img src={images.wallet} alt="Wallet" />
+              </div>
+              <div className="font-[500] mt-3">
+                <span>Available Balance</span>
+              </div>
+              <div className="text-color-primary text-[20px] md:text-[25px] font-bold">
+                <span>$70,000.00</span>
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <button className="h-[38px] w-[115px] flex justify-center items-center rounded-[4px] text-color-primary border border-color-primary hover:bg-gray-100">
+                Withdraw
+              </button>
+              <button className="h-[38px] w-[115px] flex justify-center items-center rounded-[4px] border-none bg-color-primary text-white hover:bg-color-secondary">
+                Request Loan
+              </button>
+            </div>
+          </div>
+        </div>
       </Dashboard>
     </>
   );
