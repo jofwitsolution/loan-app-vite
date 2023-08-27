dcb4b5e9d42ad3260c6017f6410a3d2020855092
Faleye Oluwafemi <faleyeoluwafemi1@gmail.com>
2023-08-27 22:36:41 +0100

    Add more component

--- src/pages/dashboard/DashboardRight.jsx
+++ src/pages/dashboard/DashboardRight.jsx
@@ -7,6 +7,17 @@ const DashboardRight = () => {
       <div className="w-max mx-auto">
         <img src={images.atm_card} alt="Atm Card" />
       </div>
+      <div className="rounded-[12px] shadow-md px-[10px] pt-[22px] pb-[15px] mt-[25px]">
+        {/* Pending Loan */}
+        <div>
+          <h3 className="font-[700] mb-4">Pending Loan</h3>
+          <div className="bg-white rounded-[4px]">Musa Mansa</div>
+        </div>
+        {/* Recent Loans */}
+        <div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/dashboard/UserOverview.jsx
+++ src/pages/dashboard/UserOverview.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Dashboard from "./Dashboard";
 import images from "../../constants/images";
+import { BsArrowUpShort, BsArrowDownShort } from "react-icons/bs";
 
 const UserOverview = () => {
   return (
@@ -26,6 +27,39 @@ const UserOverview = () => {
               <button className="h-[38px] w-[115px] flex justify-center items-center rounded-[4px] border-none bg-color-primary text-white hover:bg-color-secondary">
                 Request Loan
               </button>
+            </div>
+          </div>
+          <h2 className="text-[18px] font-[600] mb-2 mt-6">
+            Overal Statistics
+          </h2>
+          <div className="bg-white w-full shadow-md rounded-[8px] py-[16px] px-[12px] flex items-center justify-between">
+            <div className="flex flex-col">
+              <span className="font-[500]">Paid Interest</span>
+              <span className="font-[700] mt-3">$400,000</span>
+            </div>
+            <div className="flex flex-col border-l border-black pl-[8px]">
+              <span className="font-[500]">Active Loan</span>
+              <span className="font-[700] mt-3">$4,000,000</span>
+            </div>
+            <div className="flex flex-col items-center border-l border-black pl-[8px]">
+              <span className="font-[500]">Total Loan Request</span>
+              <span className="font-[700] mt-3 flex gap-[4px] items-center">
+                <span>40</span>
+                <span className="text-green-500 flex">
+                  <BsArrowUpShort />{" "}
+                  <span className="text-[10px]">5% last month</span>
+                </span>
+              </span>
+            </div>
+            <div className="flex flex-col items-center border-l border-black pl-[8px]">
+              <span className="font-[500]">Total Loan Declined</span>
+              <span className="font-[700] mt-3 flex gap-[4px] items-center">
+                <span>8</span>
+                <span className="text-red-500 flex">
+                  <BsArrowDownShort />{" "}
+                  <span className="text-[10px]">2% last month</span>
+                </span>
+              </span>
             </div>
           </div>
         </div>
